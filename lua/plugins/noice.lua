c2d6625d350126413d8a42e0a8f99389f68ef6c9
rafa-thayto <rafa.thayto@gmail.com>
2023-08-30 22:56:51 -0300

    fix(noice): finally fix the cmdline

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,6 +2,13 @@ return {
   {
     "folke/noice.nvim",
     opts = {
+      presets = {
+        bottom_search = false, -- use a classic bottom cmdline for search
+        command_palette = false, -- position the cmdline and popupmenu together
+        long_message_to_split = false, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
       cmdline = {
         view = "cmdline",
       },
