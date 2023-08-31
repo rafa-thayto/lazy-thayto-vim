324f566bb2c7e9cb5a2ced62a39e808293f12e61
Rafael Thayto <rafa.thayto@gmail.com>
2023-08-31 17:30:43 -0300

    feat(plugins): noice adjustments

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,9 +1,10 @@
 return {
   {
     "folke/noice.nvim",
+    -- enabled = false,
     opts = {
       presets = {
-        bottom_search = false, -- use a classic bottom cmdline for search
+        bottom_search = true, -- use a classic bottom cmdline for search
         command_palette = false, -- position the cmdline and popupmenu together
         long_message_to_split = false, -- long messages will be sent to a split
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
