84391ff8271a8981fb163f62b091d2d5e6eb6151
Rafael Thayto <rafa.thayto@gmail.com>
2023-08-31 20:38:33 -0300

    feat(plugins): adds nvim-ufo

---
+++ lua/plugins/nvim-ufo.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
+    opts = {
+      preview = {
+        mappings = {
+          scrollB = "<C-b>",
+          scrollF = "<C-f>",
+          scrollU = "<C-u>",
+          scrollD = "<C-d>",
+        },
+      },
+      provider_selector = function(_, filetype, buftype)
+        return (filetype == "" or buftype == "nofile") and "indent" -- only use indent until a file is opened
+          or { "treesitter", "indent" } -- if file opened, try to use treesitter if available
+      end,
+    },
+  },
+}
