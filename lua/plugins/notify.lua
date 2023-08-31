c5b8994732d392ab6c22ffb5524a82967923dfe7
rafa-thayto <rafa.thayto@gmail.com>
2023-08-30 22:57:17 -0300

    feat(notify): adjustments on notify timeout time and log levels

---
+++ lua/plugins/notify.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      level = 3,
+      render = "minimal",
+      stages = "static",
+      timeout = 500,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+    },
+  },
+}
