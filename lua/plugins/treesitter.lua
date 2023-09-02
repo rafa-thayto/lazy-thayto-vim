fdb0e35943da8dc98f63498864314ca8907a6c88
Rafael Thayto <rafa.thayto@gmail.com>
2023-09-02 15:37:24 -0300

    feat(plugins): adds nvim-treesitter-context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,26 @@ return {
   -- add more treesitter parsers
 
   {
+    "nvim-treesitter/nvim-treesitter-context",
+    lazy = true,
+    config = function()
+      require("treesitter-context").setup({
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        line_numbers = true,
+        multiline_threshold = 20, -- Maximum number of lines to show for a single context
+        trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+        -- Separator between context and content. Should be a single character string, like '-'.
+        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+        separator = nil,
+        zindex = 20, -- The Z-index of the context window
+        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+      })
+    end,
+  },
+  {
     "nvim-ts-autotag",
     config = function()
       require("nvim-ts-autotag").setup({})
@@ -12,6 +32,7 @@ return {
     dependencies = {
       "JoosepAlviste/nvim-ts-context-commentstring",
       "nvim-treesitter/nvim-treesitter-textobjects",
+      "nvim-treesitter/nvim-treesitter-context",
       "windwp/nvim-ts-autotag",
     },
     opts = function(_, opts)
