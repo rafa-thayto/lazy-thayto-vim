5246cc6be10585945f9b8195056ead264e45edad
Rafael Thayto <rafa.thayto@gmail.com>
2023-09-12 22:05:21 -0300

    feat: adds rust extras

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,7 +8,7 @@ return {
     config = true,
     keys = {
       { "<leader>hm", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon" },
-      { "<c-n>", "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = "Go to next harpoon mark" },
+      { "<c-m>", "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = "Go to next harpoon mark" },
       { "<c-e>", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Go to previous harpoon mark" },
       { "<leader>ha", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
     },
