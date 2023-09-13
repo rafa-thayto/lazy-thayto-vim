038430e810133854136d05deaf0b45cf178292ac
Rafael Thayto <rafa.thayto@gmail.com>
2023-09-13 20:36:04 -0300

    feat(plugins): adds haskell

---
+++ lua/plugins/haskell.lua
@@ -0,0 +1,10 @@
+return {
+  -- {
+  --   "mrcjkb/haskell-tools.nvim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --   },
+  --   branch = "2.x.x", -- Recommended
+  --   ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
+  -- },
+}
