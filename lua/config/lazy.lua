0c92a3ea8ed19387b415b2685897738b5e8ae701
Rafael Thayto <rafa.thayto@gmail.com>
2024-02-21 11:48:22 -0300

    fix(plugins): adds vim-tmux-navigator

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,10 +18,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.docker" },
-    { import = "lazyvim.plugins.extras.dap.core" },
+    -- { import = "lazyvim.plugins.extras.dap.core" },
     -- { import = "lazyvim.plugins.extras.lang.go" },
     -- { import = "lazyvim.plugins.extras.lang.rust" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
   },
