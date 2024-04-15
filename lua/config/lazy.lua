eff046f046d7204df0d30b600d3965abd813fe93
Rafael Thayto <rafa.thayto@gmail.com>
2024-04-15 16:45:46 -0300

    feat: adds neotest and dap

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.docker" },
-    -- { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.test.core" },
+    { import = "lazyvim.plugins.extras.dap.core" },
     -- { import = "lazyvim.plugins.extras.lang.go" },
     -- { import = "lazyvim.plugins.extras.lang.rust" },
     -- import/override with your plugins
