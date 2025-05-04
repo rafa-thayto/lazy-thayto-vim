9bc9893641d8163724fbacd4a75669758dad6c3b
Darlan Jr. <kelgaxd@gmail.com>
2025-05-04 03:24:59 -0400

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.snacks_animate = false
+
+-- Configuração para ativar o suporte ao clipboard
+vim.opt.clipboard = "unnamedplus"
